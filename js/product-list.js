4ad4dd98ac1cf48ca3ec7a64c059f8d9108cc8fa
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-04-24 18:47:22 +0200

    remove all the gta and hotjar links
    it was causing some issues to the consoles

--- js/product-list.js
+++ js/product-list.js
@@ -1,1 +1,9 @@
-const API_URL = '';
+const API_URL = 'imaginary-growler.flywheelsites.com/wp-json/wc/store/products';
+const productsContainer = document.querySelector('.results');
+
+async function getGamehubProducts() {
+  try {
+    const response = await fetch(API_URL);
+    console.log(API_URL);
+  } catch (error) {}
+}
