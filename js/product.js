1a79c610bbd4538650b210e25830dcaf3547823e
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-05-01 20:15:11 +0200

    made api call and made few changes to the images

--- js/product.js
+++ js/product.js
@@ -1,6 +1,6 @@
 const API_URL =
   'https://nsebonkili-9438ad.ingress-alpha.easywp.com/wp-json/wc/store/products';
-const productsContainer = document.querySelector('.results');
+let productsContainer = document.querySelector('.results');
 async function getGamehubProducts() {
   try {
     const response = await fetch(API_URL);
@@ -8,9 +8,11 @@ async function getGamehubProducts() {
     const productsData = responseJSON;
     productsContainer.innerHTML = '';
     for (let i = 0; i < productsData.length; i++) {
+      console.log(productsData[i].images[0].src);
       productsContainer.innerHTML += `
      <div class="card">
      <div class="heading">
+     <img src=${productsData[i].images[0].src}>
      <a href="product-details.html?id=${productsData[i].id}"><strong> Product Name: ${productsData[i].name}</strong></a>
      </div>
      <p>Description : ${productsData[i].description}</p>
