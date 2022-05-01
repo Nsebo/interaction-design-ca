72e1260fa2bd7c8f3047c141a2f20eda45ecbee4
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-05-01 21:14:38 +0200

    work in progress

--- js/product-details.js
+++ js/product-details.js
@@ -8,3 +8,27 @@ function menutoggle() {
     MenuItems.style.maxHeight = '0px';
   }
 }
+const querystring = document.location.search;
+const params = new URLSearchParams(querystring);
+const id = params.get('id');
+console.log(id);
+// Wordpress returns all products for some reason
+const API_URL = `https://nsebonkili-9438ad.ingress-alpha.easywp.com/wp-json/wc/store/products?id=${id}`;
+console.log(API_URL);
+const gameDetails = document.querySelector('.game-details');
+let singleGameDetails;
+async function getGameDetails() {
+  try {
+    const response = await fetch(API_URL);
+    const allGameDetails = await response.json();
+
+    // Iterate through response to find the actual single product
+    for (let i = 0; i < allGameDetails.length; i++) {
+      if (allGameDetails[i].id === Number(id)) {
+        singleGameDetails = allGameDetails[i];
+      }
+    }
+    console.log(singleGameDetails);
+  } catch (error) {}
+}
+getGameDetails();
